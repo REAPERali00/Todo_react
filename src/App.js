f6c563da26b496ca0709ccd21982bee2f627a8a7
Alireza Jadidi <120317445+REAPERali00@users.noreply.github.com>
2023-12-20 17:56:10 -0500

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="center">
+      <h1>Hello world</h1>
+      <p className="Paragraph">This is a paragraph</p>
+      <h2>this is a heading without the center class</h2>
+    </div>
+  );
+}
+
+export default App;
