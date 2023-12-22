b07783cce8aa6c894893bc481b382effc3cc12a9
Alireza Jadidi <120317445+REAPERali00@users.noreply.github.com>
2023-12-21 19:06:16 -0500

    Updated practice in css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import "./App.css";
-
+import meme from "./assets/7-o.jpg";
 function App() {
   return (
     <div className="center">
       <h1>Hello world</h1>
       <p className="Paragraph">This is a paragraph</p>
       <h2>this is a heading without the center class</h2>
+      <img src={meme} alt="meme" />
+      <p className="borders">this is to test borders</p>
+      <p className="margins">this is to test margin</p>
+      <p className="Outlined">this is to test border outline.</p>
     </div>
   );
 }
